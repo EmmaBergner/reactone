88f8e62a06b5395029bfb8d55af1cf27e64c00ec
EmmaBergner <bergner.emma@gmail.com>
2022-06-16 12:24:15 +0000

    text to page

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './css/Sidebar.module.css';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <a href="www.google.com" >My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Paintings</a>
+    </div>
+  );
+}
+
+export default Sidebar;
